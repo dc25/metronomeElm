8448fa2cc9de2006942fbd54a75fbe58940708db
Dave Compton <davecompton7@gmail.com>
2016-02-13 22:17:48 +0000

    replacing flute sound with tick and tock

--- Metronome.elm
+++ Metronome.elm
@@ -1,3 +1,5 @@
+module Metronome where 
+
 import Time exposing (..)
 import Html exposing (..)
 import Graphics.Collage exposing (..)
@@ -12,9 +14,10 @@ update _ model = model + 1
 -- view : Model -> Html
 view m = show m
 
-
-
 -- main : Signal Html
-main =    Signal.foldp update 0 (every second)
+main =    Signal.foldp update 0 (every (second))
        |> Signal.map view 
 
+
+port tick : Signal Int
+port tick = foldp (\_ c -> c+1) 0 (every (2*second))
