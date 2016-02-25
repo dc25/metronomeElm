481c09509b62fc24f0e39683890ed2effb2b07cb
Dave Compton <davecompton7@gmail.com>
2016-02-25 03:41:18 +0000

    added in a svg elm logo just as a placeholder

--- Metronome.elm
+++ Metronome.elm
@@ -2,12 +2,15 @@ module Metronome where
 
 import Color exposing (..)
 import Time exposing (..)
-import Html exposing (..)
+import Html exposing (Html, li, text, ul, div, button, fromElement)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Signal exposing (..)
+import Mouse 
+import Svg exposing (svg)
+import Svg.Attributes exposing (version, viewBox, x, y, fill,points, transform)
 import Mouse 
 
 dt = 0.01
@@ -80,6 +83,7 @@ view model =
                           [ onClick control.address ToggleStarted ]
                           [ Html.text (if model.started then "Stop" else "Start") ]
                       ]
+      , div floatLeft [ logo ]
       , div floatLeft [collage 500 800 [ pendulum ] |> fromElement]
       ]
 
@@ -107,4 +111,21 @@ port leftRight = leftRightSignal
 main = Signal.map view modelSignal 
 
 floatLeft = [ style [ ("float", "left") ] ]
+
+
+logo : Html
+logo =
+  svg [ version "1.1", x "0", y "0", Svg.Attributes.width "120", Svg.Attributes.height "120", viewBox "0 0 400 400" ]
+    [ Svg.polygon [ fill "#F0AD00", points "161.649,152.782 231.514,82.916 91.783,82.916" ] []
+    , Svg.polygon [ fill "#7FD13B", points "8.867,0 79.241,70.375 232.213,70.375 161.838,0" ] []
+    , Svg.rect
+        [ fill "#7FD13B", x "192.99", y "107.392", Svg.Attributes.width "107.676", Svg.Attributes.height "108.167"
+        , transform "matrix(0.7071 0.7071 -0.7071 0.7071 186.4727 -127.2386)"
+        ]
+        []
+    , Svg.polygon [ fill "#60B5CC", points "323.298,143.724 323.298,0 179.573,0" ] []
+    , Svg.polygon [ fill "#5A6378", points "152.781,161.649 0,8.868 0,314.432" ] []
+    , Svg.polygon [ fill "#F0AD00", points "255.522,246.655 323.298,314.432 323.298,178.879" ] []
+    , Svg.polygon [ fill "#60B5CC", points "161.649,170.517 8.869,323.298 314.43,323.298" ] []
+    ]
 
