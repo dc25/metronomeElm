3c46ab2b9eba9235ac52a19cd2b28845205b0d40
Dave Compton <davecompton7@gmail.com>
2016-02-26 06:39:28 +0000

    game names to some constants

--- Metronome.elm
+++ Metronome.elm
@@ -1,27 +1,31 @@
 module Metronome where 
 
 import Color exposing (..)
-import Time exposing (..)
+import String exposing (join)
+import Time exposing (every, second)
 import Html exposing (Html, li, text, ul, div, button, fromElement)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-import Graphics.Collage exposing (..)
-import Graphics.Element exposing (..)
-import Signal exposing (..)
-import Mouse 
+import Html.Attributes as HA
+import Html.Events exposing (onClick)
+import Graphics.Collage exposing (collage, rotate, move, filled, ngon, circle, traced, segment, group, defaultLine)
 import Svg exposing (svg)
-import Svg.Attributes exposing (version, viewBox, cx, cy, r, x, y, x1, y1, x2, y2, fill,points, transform)
-import Mouse 
+import Svg.Attributes exposing (version, viewBox, cx, cy, r, x, y, x1, y1, x2, y2, fill,points, transform, style, width, height)
 
 dt = 0.01
 scale = 100
+gravity = -9.81
+
+w = 500
+h = 700
+
+pivotDiameter = 8
+fobDiameter = 12
+arrowSize = 10
 
 type alias Model =
   { angle : Float
   , angVel : Float
   , length : Float
   , slideRatio : Float
-  , gravity : Float
   , started : Bool
   }
 
@@ -30,7 +34,6 @@ init =
   , angVel = 0.0
   , length = 2
   , slideRatio = 0.3
-  , gravity = -9.81
   , started = True
   }
 
@@ -41,7 +44,7 @@ update action model =
   case action of
     Tick -> 
       if model.started then 
-        let angAcc = 1.0 * (model.gravity / (model.slideRatio * model.length)) * sin (model.angle)
+        let angAcc = 1.0 * (gravity / (model.slideRatio * model.length)) * sin (model.angle)
             angVel' = model.angVel + angAcc * dt
             angle' = model.angle + angVel' * dt
         in { model | angle = angle' , angVel = angVel' }
@@ -63,12 +66,15 @@ view model =
         (group
           [ segment ( 0, metronomeLength) (0, pendulumLength)
               |> traced { defaultLine | width = 2, color = red }
-          , circle 8
+
+          , circle pivotDiameter
             |> filled blue
-          , circle 12
+
+          , circle fobDiameter
             |> filled purple
             |> move ( 0, (model.slideRatio) * pendulumLength)
-          , ngon 3 10
+
+          , ngon 3 arrowSize
             |> filled green
             |> rotate (-pi/2)
             |> move ( 0, metronomeLength)
@@ -76,25 +82,23 @@ view model =
 
     svgPendulum = 
       [ Svg.g 
-        [ Svg.Attributes.transform ("rotate(" ++ toString (model.angle * 180/pi)  ++ ")") ]
+        [ transform ("rotate(" ++ toString (model.angle * 180/pi)  ++ ")") ]
         [ Svg.line [ y1 (toString metronomeLength)
                    , y2 (toString pendulumLength)
-                   , Svg.Attributes.style "stroke:red;stroke-width:2" ] []
+                   , style "stroke:red;stroke-width:2" ] []
 
-        , Svg.circle [ r (toString 8)
+        , Svg.circle [ r (toString pivotDiameter)
                      , fill "blue"
-                     ]
-          []
+                     ] []
 
         , Svg.circle [ cy (toString (model.slideRatio * pendulumLength))
-                     , r (toString 12)
+                     , r (toString fobDiameter)
                      , fill "purple"
-                     ]
-          []
+                     ] []
 
-        , Svg.polygon [ points "0,-10 10,0 -10,0"
+        , Svg.polygon [ points "0,-arrowSize arrowSize,0 -arrowSize,0"
                       , fill "lime" 
-                      , Svg.Attributes.transform ("translate(0 " ++ toString metronomeLength  ++ ")")
+                      , transform ("translate(0 " ++ toString metronomeLength  ++ ")")
                       ] []
         ]
       ]
@@ -104,14 +108,26 @@ view model =
                           [ onClick control.address ToggleStarted ]
                           [ Html.text (if model.started then "Stop" else "Start") ]
                       ]
-      , div floatLeft [ svg [ version "1.1", x "0", y "0", Svg.Attributes.width "500", Svg.Attributes.height "700", viewBox "-250 -350 500 700" ] svgPendulum ]
-      , div floatLeft [collage 500 700 [ collagePendulum ] |> fromElement]
+
+      , div floatLeft [ svg 
+                          [ version "1.1"
+                          , width (toString w)
+                          , height (toString h)
+                          , join " " [-w//2 |> toString
+                                     ,-h//2 |> toString
+                                     ,    w |> toString
+                                     ,    h |> toString ] |> viewBox
+                          ] 
+                          svgPendulum 
+                      ]
+
+      , div floatLeft [ collage w h [ collagePendulum ] |> fromElement]
       ]
 
 
 control = Signal.mailbox NoOp
 
-tickSignal =   (every (dt * second)) |> map (always Tick)
+tickSignal =   (every (dt * second)) |> Signal.map (always Tick)
 
 actionSignal = Signal.mergeMany [tickSignal, control.signal]
 
@@ -123,11 +139,10 @@ leftRightSignal =
   |> Signal.map (\model -> model.angle < 0) 
   |> Signal.dropRepeats 
 
-
 port leftRight : Signal Bool
 port leftRight = leftRightSignal
 
 main = Signal.map view modelSignal 
 
-floatLeft = [ style [ ("float", "left") ] ]
+floatLeft = [ HA.style [ ("float", "left") ] ]
 
